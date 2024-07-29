0c87c83b756661ac26c09277ee1bdff4e6e90772
abshravank <abshravank@gmail.com>
2024-07-29 17:11:28 +0000

    day1 completed

---
+++ 30_days_python/day 1.py
@@ -0,0 +1,43 @@
+#Exersize 1
+first_name="A B Shravan"
+last_name="Krishna"
+full_name="A B Shravan Krishna"
+country="India"
+city="Kollam"
+age=22
+year=2024
+#multiple variables declared in a single line
+is_married,is_true,is_light_on=False,True,"yes"
+print(first_name,last_name,full_name,country,city,age,year,is_married,is_true,is_light_on)
+#Exersize 2
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(len(first_name))
+print(len(last_name))
+num_one=5
+num_two=4
+total=num_one+num_two
+diff=num_one-num_two
+product=num_one*num_two
+division=num_one/num_two
+remainder=num_one%num_two
+exp=num_one**num_two
+floor_division=num_one//num_two
+radius=30
+area_of_circle=3.14*radius**2
+circum_of_circle=2*3.14*radius
+new_radius=input("enter the radius")
+area_of_circle=3.14*int(new_radius)**2
+print(area_of_circle)
+new_name,new_country,age=input("enter the name"),input("enter the country"),input("enter the age")
+print(new_name)
+print(new_country)
+print(age)
