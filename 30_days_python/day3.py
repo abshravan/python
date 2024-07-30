2718f49e90826d7ba8c5e21152a86994307fc6ca
abshravank <abshravank@gmail.com>
2024-07-30 11:36:17 +0000

    18 questions completed

---
+++ 30_days_python/day3.py
@@ -0,0 +1,43 @@
+#Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'.
+str1,str2,str3,str4="Thrirty ","Days ","Of ","Python "
+print(str1+str2+str3+str4)
+#Concatenate the string 'Coding', 'For' , 'All' to a single string, 'Coding For All'
+str5,str6,str7="Coding ","For ","All "
+print(str5+str6+str7)
+#Declare a variable named company and assign it to an initial value "Coding For All".
+company="Coding For  All"
+#Declare a variable named company and assign it to an initial value "Coding For All".
+print(company)
+#Print the length of the company string using len() method and print().
+print(len(company))
+#Change all the characters to uppercase letters using upper() method.
+print(upper(company))
+#Change all the characters to lowercase letters using lower() method.
+print(lower(company))
+#Use capitalize(), title(), swapcase() methods to format the value of the string Coding For All.
+print(capitalize(compay),title(company),swapcase(company))
+#Cut(slice) out the first word of Coding For All string.
+print(company[7:1])
+#Check if Coding For All string contains a word Coding using the method index, find or other methods.
+print(company.find("Coding"))
+#Replace the word coding in the string 'Coding For All' to Python.
+print(company.replace("Coding","Python"))
+#Change Python for All to Python for Everyone using the replace method or other methods.
+print(company.replace("All","Everyone"))
+#Split the string 'Coding For All' using space as the separator (split()) .
+print(compant.split(" "))
+# "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon" split the string at the comma.
+str8="Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+print(str8.split(","))
+#What is the character at index 0 in the string Coding For All.
+print(company[0])
+#What is the last index of the string Coding For All.
+print(company[-1])
+#What is the character at index 0 in the string Coding For All.
+print(company[0])
+#What is the last index of the string Coding For All.
+print(company[-1])
+#What is the character at index 10 in "Coding For All" string.
+print(company[10])
+#Create an acronym or an abbreviation for the name 'Python For Everyone'.
+print(company[0:1],company[7:8],company[11;12])
