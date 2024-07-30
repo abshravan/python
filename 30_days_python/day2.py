163fb5ec3384de45956d4ea6e82487492c917e59
abshravank <abshravank@gmail.com>
2024-07-30 07:01:27 +0000

    basic opertions done

---
+++ 30_days_python/day2.py
@@ -0,0 +1,29 @@
+# exersize 3
+age=int(22)
+height=float(180.4)
+z1=complex(2,-2) #declaring a complex number
+#area of a triangle
+base= int(input("enter the base"))
+height=int(input("enter the height"))
+area_of_triangle=0.5*base*height
+print(area_of_triangle)
+#perimeter of a triangle
+a=int(input("enter the first side"))
+b=int(input("enter the second side"))
+c=int(input("enter the third side"))
+perimeter_of_triangle=a+b+c
+print("perimeter of triangle is ",perimeter_of_triangle)
+
+d=int(input("enter the length"))
+e=int(input("enter the width"))
+area_of_rectangle=d*e
+print("area of rectangle is ",area_of_rectangle)
+perimeter_of_rectangle=2*(d+e)
+print("perimeter of rectangle is ",perimeter_of_rectangle)
+
+radius=int(input("enter the radius"))
+area_of_circle=3.14*radius**2
+print("area of circle is ",area_of_circle)
+circum_of_circle=2*3.14*radius
+print("circumference of circle is ",circum_of_circle)
+
