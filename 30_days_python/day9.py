faad04c8a153ccf44bb190dd3c0d3af8da240362
abshravank <abshravank@gmail.com>
2024-08-04 09:19:08 +0000

    exersize 2 completed

--- 30_days_python/day9.py
+++ 30_days_python/day9.py
@@ -76,3 +76,27 @@ for i in range(0,101):
 for i in range(0,101):
     if i%2==1:
         print(i)
+
+#Exercises: Level 2
+"""
+Use for loop to iterate from 0 to 100 and print the sum of all numbers.
+The sum of all numbers is 5050.
+"""
+sum=0
+for i in range(0,101):
+    sum+=i
+print(sum)
+
+"""
+Use for loop to iterate from 0 to 100 and print the sum of all evens and the sum of all odds.
+The sum of all evens is 2550. And the sum of all odds is 2500.
+"""
+sum1=0
+sum2=0
+for i in range(0,101):
+    if i%2==0:
+        sum1+=i
+    else:
+        sum2+=i
+print(sum1)
+print(sum2)
