d6689297ad114e0a01c3ec0a52c4d616a1b7500b
abshravank <abshravank@gmail.com>
2024-08-04 13:28:50 +0000

    update

--- 30_days_python/day9.py
+++ 30_days_python/day9.py
@@ -100,3 +100,19 @@ for i in range(0,101):
         sum2+=i
 print(sum1)
 print(sum2)
+
+#Exercises: Level 3
+#Go to the data folder and use the countries.py file. Loop through the countries and extract all the countries containing the word land.
+
+
+#This is a fruit list, ['banana', 'orange', 'mango', 'lemon'] reverse the order using loop.
+
+
+
+
+"""
+Go to the data folder and use the countries_data.py file.
+What are the total number of languages in the data
+Find the ten most spoken languages from the data
+Find the 10 most populated countries in the world
+""
